06d571239f55e17f87588ede7f686369969dc033
mori-5593 <brook2915@icloud.com>
2025-10-30 15:29:45 +0900

    rails geberate時に不要なファイルが自動生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -2,26 +2,18 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TeclogApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+# コマンドで余分なファイルが自動生成されるのを制限する
+    config.generators do |g|
+      g.assets false # css.jsファイルが無駄に生成されるのを防ぐ
+      g.helper false # helperファイルという主にviewファイルで使えるようなメソッドを管理するためのファイルを自動で生成されるのを防ぐ
+      g.test_framework :rspec # RSpecというテストフレームを実施するため、他のテストフレームは生成しない。
+    end
   end
 end
