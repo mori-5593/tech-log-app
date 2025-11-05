1fb3b2b655c1220fdbb7803515f3df1ce0fcb397
mori-5593 <brook2915@icloud.com>
2025-11-05 17:18:13 +0900

    学習ログ詳細ページを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
     nickname { 'テスト太郎' }
-    sequence :email do |_n| # メールを自動的に連番、数字を少しずつ変えて設定することができる
-      'test#(n)@example.com' # test1@~~ test2@~~ というように作成してくれる
-    end
+    # メールを自動的に連番、数字を少しずつ変えて設定することができる
+    # test1@~~ test2@~~ というように作成してくれる
+    sequence(:email) { |n| "test#{n}@example.com" }
     password { '111111' }
     password_confirmation { '111111' } # 2回入力してミスを防ぐもの
   end
